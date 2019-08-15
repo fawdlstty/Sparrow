d1f19b849777539131fbadfdbaf8ccf614f25259
fawdlstty <f@fawdlstty.com>
2019-08-15 23:56:30 +0800

    ws module

---
+++ src/TestServer/WSTestModule.cs
@@ -0,0 +1,22 @@
+﻿using Newtonsoft.Json.Linq;
+using SparrowServer.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestServer {
+	[WSModule ("webservice module")]
+	public class WSTestModule {
+		[JWTRequest]
+		public static WSTestModule _check_auth (JObject _jwt) {
+			return new WSTestModule { n = 100 };
+		}
+
+		//[WSOnMsg]
+		public void on_msg (byte [] _data) {
+			
+		}
+
+		private int n = 0;
+	}
+}
